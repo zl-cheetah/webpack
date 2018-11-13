155c88a94178061589204da19d4a45fce41399b2
zl-cheetah <18895686173@163.com>
2018-11-14 00:06:25 +0800

    继续完善webpack加载，并准备加入boot。2018/11/14

--- src/index.js
+++ src/index.js
@@ -14,4 +14,18 @@ import printMe from './print.js';
         return element;
     }
 
-document.body.appendChild(component());
+let element = component(); // 当 print.js 改变导致页面重新渲染时，重新获取渲染的元素
+
+document.body.appendChild(element);
+
+if (module.hot) {
+       module.hot.accept('./print.js', function() {
+           console.log('Accepting the updated printMe module!');
+
+           document.body.removeChild(element);
+           
+           element = component(); // 重新渲染页面后，component 更新 click 事件处理
+
+           document.body.appendChild(element);
+    })
+}
